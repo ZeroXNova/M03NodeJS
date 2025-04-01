7e94f6d651a8e6f088f4495160ed1d51d6918538
James Klick <jklick@ivytech.edu>
2025-04-01 17:54:55 -0400

    Add files via upload

---
+++ node-crash-course/people.js
@@ -0,0 +1,8 @@
+const people = ['Yoshi', 'Ryu', 'Chun-li', 'Mario'];
+const ages = [20, 25, 30, 35];
+
+// console.log(people);
+
+module.exports = {
+    people, ages
+};
---
+++ node-crash-course/test.js
@@ -0,0 +1,10 @@
+//const name = 'yoshi';
+
+//console.log(name);
+
+const greet = (name1) => {
+    console.log(`Hello, ${name1}`);
+}
+
+greet('mario');
+greet('yoshi');
