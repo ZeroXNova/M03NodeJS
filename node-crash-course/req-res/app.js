d9fe8f935eeb06d196586135d6ed6cb1e6259491
Jim Klick <jklick@ivytech.edu>
2025-04-02 16:46:00 -0400

    cre ated create page

--- node-crash-course/req-res/app.js
+++ node-crash-course/req-res/app.js
@@ -10,21 +10,18 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/',(req,res) => {
-    // res.send('<p>Home Page</p>');
     res.render('index');
 });
 
 app.get('/about',(req,res) => {
-    // res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req,res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req,res) => {
+    res.render('create');
+})
 
 // 404
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
