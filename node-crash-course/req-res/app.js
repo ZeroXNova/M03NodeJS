a98b9dfca9df9bfd6381b6b1214d56a3f38dd7c5
Jim Klick <jklick@ivytech.edu>
2025-04-02 16:33:08 -0400

    installed ejs

--- node-crash-course/req-res/app.js
+++ node-crash-course/req-res/app.js
@@ -3,12 +3,15 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/',(req,res) => {
     // res.send('<p>Home Page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about',(req,res) => {
