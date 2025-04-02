e0156d6843d2e5cdf579efa975bea6bf6c457a3a
Jim Klick <jklick@ivytech.edu>
2025-04-02 13:51:57 -0400

    added about-me redirect
    updated files

--- node-crash-course/req-res/server.js
+++ node-crash-course/req-res/server.js
@@ -1,8 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello');
+    });
+
+    greet();
+    greet();
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
@@ -11,12 +22,20 @@ const server = http.createServer((req, res) => {
     switch(req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.setHeader('Location', '/about');
+            res.statusCode = 301;
+            res.end();
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
