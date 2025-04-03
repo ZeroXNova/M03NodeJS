56c3f41b8a7ee678eca24588363c740258c47035
Jim Klick <jklick@ivytech.edu>
2025-04-03 14:24:19 -0400

    updated 404 routing

--- node-crash-course/req-res/app.js
+++ node-crash-course/req-res/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
@@ -25,43 +26,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// mongoose & mongo tests
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-    title: 'new blog',
-    snippet: 'about my new blog',
-    body: 'more about my new blog'
-    })
-
-    blog.save()
-    .then(result => {
-        res.send(result);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-    .then(result => {
-        res.send(result);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('5ea99b49b8531f40c0fde689')
-    .then(result => {
-        res.send(result);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-
 app.get('/', (req, res) => {
     res.redirect('/blogs');
 });
@@ -70,59 +34,8 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
-// index
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-        res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-});
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then(() => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
-
-app.get('/blogs/:id', (req,res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', {blog: result, title: 'Blog Details'});
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-app.delete('/blogs/:id', (req,res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-    .then(result => {
-        res.json({ redirect: '/blogs' });
-    })
-    .catch(err => {
-        console.log(err);
-    });
-});
-
 // blog routes
-
-
-
+app.use('/blogs', blogRoutes);
 
 // 404 page
 app.use((req, res) => {
